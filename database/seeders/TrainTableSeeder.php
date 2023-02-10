ba96e731e584203302d19ecd02f644e3a392d481
MyUsername <my@email.com>
2023-02-10 17:27:37 +0100

    trying to use faker

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainTableSeeder extends Seeder
 {
@@ -12,10 +14,23 @@ class TrainTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         $this ->call([
             TrainTableSeeder::class
         ]);
+
+        for ($i=0; $i < 100 ; $i++) { 
+            $new_train_object = new Train();
+            $new_train_object->Azienda = $faker->sentence(5,true);
+            $new_train_object->Stazione_di_partenza= $faker->sentence(5,true);
+            $new_train_object->Orario_di_partenza= $faker->dateTime();
+            $new_train_object->Orario_di_arrivo= $faker->dateTime();
+            $new_train_object->Codice_Treno= $faker->ean8();
+            $new_train_object->Numero_Carrozze= $faker->randomDigit();
+            $new_train_object->In_orario= $faker->boolean();
+            $new_train_object->Cancellato= $faker->boolean();
+            $new_train_object->save();
+        }
     }
 }
